6c1811d6ecea7d1a34155e34826eeae3c3280596
Synidle <73420926+Synidle@users.noreply.github.com>
2025-03-09 15:36:19 +0000

    conditional question

--- behaviour.js
+++ behaviour.js
@@ -1,6 +1,24 @@
+const childrenRadio = document.getElementsByName("has-children");
+const petsRadio = document.getElementsByName("other-pets");
 const submitButton = document.getElementById("submit");
+
+for (let rb of childrenRadio)
+    rb.addEventListener("change", (event) => 
+        toggleConditional(event, "div-child-age"));
+for (let rb of petsRadio)
+    rb.addEventListener("change", (event) =>
+        toggleConditional(event, "div-species"));
 
 submitButton.addEventListener("click", (event) => {
     event.preventDefault();
 });
 
+function toggleConditional(event, id)
+{
+    let target = document.getElementById(id);
+
+    if (event.target.value == "yes")
+        target.style.display = "block";
+    else
+        target.style.display = "none";
+}
