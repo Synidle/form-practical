0514e073902d8770a5921052fa1ff5c4da5af054
Synidle <73420926+Synidle@users.noreply.github.com>
2025-03-28 14:12:06 +0000

    save data

--- behaviour.js
+++ behaviour.js
@@ -1,6 +1,18 @@
 const childrenRadio = document.getElementsByName("has-children");
 const petsRadio = document.getElementsByName("other-pets");
-const submitButton = document.getElementById("submit");
+const form = document.getElementById("form");
+//const submitButton = document.getElementById("submit");
+
+let formObj = {
+    name: "",
+    email: "",
+    phone: "",
+    outsideSpace: false,
+    children: false,
+    childAge: 0,
+    pets: false,
+    petSpecies: ""
+}
 
 for (let rb of childrenRadio)
     rb.addEventListener("change", (event) => 
@@ -9,9 +21,28 @@ for (let rb of petsRadio)
     rb.addEventListener("change", (event) =>
         toggleConditional(event, "div-species", "species"));
 
-submitButton.addEventListener("click", (event) => {
-    //event.preventDefault();
-    //alert("Submit Succesfully!");
+form.addEventListener("submit", function(event) {
+    console.log("submit");
+    formObj.name = document.getElementById("name").value;
+    formObj.email = document.getElementById("email").value;
+    formObj.phone = document.getElementById("phone").value;
+    formObj.outsideSpace = document.getElementById("yes-outside-space").checked;
+    formObj.children = document.getElementById("yes-children").checked;
+    formObj.childAge = document.getElementById("child-age").value;
+    formObj.pets = document.getElementById("yes-pets").checked;
+    formObj.petSpecies = document.getElementById("species").value;
+    console.log(formObj);
+
+    if (localStorage.getItem("formData") == null) {
+        localStorage.setItem("formData", JSON.stringify([formObj]));
+    }
+    else {
+        let formData = [];
+        formData = JSON.parse(localStorage.getItem("formData"));
+        formData.push(formObj);
+        localStorage.setItem("formData", JSON.stringify(formData));
+    }
+
 });
 
 function toggleConditional(event, divID, inputID)
@@ -28,5 +59,6 @@ function toggleConditional(event, divID, inputID)
     {
         div.style.display = "none";
         input.required = false;
+        input.value = "";
     }
 }
